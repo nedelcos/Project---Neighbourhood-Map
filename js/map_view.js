749833ec573cc0e0ef032d6f829eefe5120dffff
Daniel Nedelcu <nede.dan@gmail.com>
2016-12-08 15:41:20 +0200

    created map and added style to map

--- js/map_view.js
+++ js/map_view.js
@@ -1,1 +1,13 @@
+//Global variables
 var map;
+var parks = []; //Stores all parks in this array
+
+function initMap() {
+  //constructor creates a new map, only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 44.426780, lng: 26.104116},
+    zoom: 13,
+    styles: styles,
+    mapTypeControl: false
+  });
+}
