e984865ba7f6296040f055de925a14d29bfae10a
Daniel Nedelcu <nede.dan@gmail.com>
2016-12-08 14:31:32 +0200

    added park locations

---
+++ js/parksDB.js
@@ -0,0 +1,141 @@
+// Parks in Bucharest - Romania
+
+var parkLocations = [{
+    title: "Morarilor Park",
+    location: {
+        lat: 44.440521,
+        lng: 26.170378
+    }
+}, {
+    title: "National Park",
+    location: {
+        lat: 44.434883,
+        lng: 26.147718
+    }
+}, {
+    title: "Titan Park",
+    location: {
+        lat: 44.422879,
+        lng: 26.156130
+    }
+}, {
+    title: "Vacaresti Natural Park",
+    location: {
+        lat: 44.399431,
+        lng: 26.133342
+    }
+}, {
+    title: "Plumbuita Park",
+    location: {
+        lat: 44.469806,
+        lng: 26.135359
+    }
+}, {
+    title: "Bordei Park",
+    location: {
+        lat: 44.471531,
+        lng: 26.093903
+    }
+}, {
+    title: "Herastrau Park",
+    location: {
+        lat: 44.479186,
+        lng: 26.080599
+    }
+}, {
+    title: "Bazilescu Park",
+    location: {
+        lat: 44.488668,
+        lng: 26.034508
+    }
+}, {
+    title: "Kiseleff Park",
+    location: {
+        lat: 44.457677,
+        lng: 26.083603
+    }
+}, {
+    title: "Cismigiu Park",
+    location: {
+        lat: 44.436660,
+        lng: 26.091070
+    }
+}, {
+    title: "Botanic Garden",
+    location: {
+        lat: 44.437273,
+        lng: 26.062660
+    }
+}, {
+    title: "The Youth Park",
+    location: {
+        lat: 44.406878,
+        lng: 26.104460
+    }
+}, {
+    title: "Carol Park",
+    location: {
+        lat: 44.413183,
+        lng: 26.095533
+    }
+}, {
+    title: "Drumul Taberei Park",
+    location: {
+        lat: 44.420024,
+        lng: 26.032190
+    }
+}, {
+    title: "Giulesti Park",
+    location: {
+        lat: 44.460531,
+        lng: 26.043005
+    }
+}, {
+    title: "Crangasi Park",
+    location: {
+        lat: 44.451735,
+        lng: 26.043692
+    }
+}, {
+    title: "Pantelimon Park",
+    location: {
+        lat: 44.436880,
+        lng: 26.203208
+    }
+}, {
+    title: "Constantin Brancusi Park",
+    location: {
+        lat: 44.429306,
+        lng: 26.164799
+    }
+}, {
+    title: "Gheorghe Patrascu Park",
+    location: {
+        lat: 44.431298,
+        lng: 26.149778
+    }
+}, {
+    title: "Opera Park",
+    location: {
+        lat: 44.436064,
+        lng: 26.077273
+    }
+}, {
+    title: "Izvor Park",
+    location: {
+        lat: 44.431637,
+        lng: 26.087916
+    }
+}, {
+    title: "Grozavesti Park",
+    location: {
+        lat: 44.436203,
+        lng: 26.056416
+    }
+}, {
+    title: "Queen Marie Park",
+    location: {
+        lat: 44.457815,
+        lng: 26.066008
+    }
+}];
