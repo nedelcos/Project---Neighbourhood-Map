5f2754e7f16cdb43e2e2abc57c25b21bb0776092
Daniel Nedelcu <nede.dan@gmail.com>
2016-12-08 16:25:38 +0200

    added markers (parks)

--- js/map_view.js
+++ js/map_view.js
@@ -2,6 +2,8 @@
 var map;
 var parks = []; //Stores all parks in this array
 
+
+// initMap() loads the map with the specified parameters
 function initMap() {
   //constructor creates a new map, only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
@@ -10,4 +12,31 @@ function initMap() {
     styles: styles,
     mapTypeControl: false
   });
+
+
+  var position;
+  var title;
+  var defaultMarker = makeMarkerIcon();
+
+  for (i = 0; i < parkLocations.length; i++) {
+    position = parkLocations[i].location;
+    title = parkLocations[i].title;
+
+    var marker = new google.maps.Marker({
+      position: position,
+      icon: defaultMarker,
+      title: title,
+      id: i
+    });
+
+    parks.push(marker);
+
+    parks[i].setMap(map);
+  };
+
+}
+
+function makeMarkerIcon () {
+  var markerImage = new google.maps.MarkerImage('img/tree.png');
+  return markerImage;
 }
