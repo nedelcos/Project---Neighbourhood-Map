54c2edafeb486ca49359a3b53121b13a7db563f6
Daniel Nedelcu <nede.dan@gmail.com>
2016-12-09 16:10:20 +0200

    added nav

--- js/parksDB.js
+++ js/parksDB.js
@@ -2,138 +2,161 @@
 
 var parkLocations = [{
     title: "Morarilor Park",
+    sector: "Sector 3",
     location: {
         lat: 44.440521,
         lng: 26.170378
     }
 }, {
     title: "National Park",
+    sector: "Sector 2",
     location: {
         lat: 44.434883,
         lng: 26.147718
     }
 }, {
     title: "Titan Park",
+    sector: "Sector 3",
     location: {
         lat: 44.422879,
         lng: 26.156130
     }
 }, {
     title: "Vacaresti Natural Park",
+    sector: "Sector 4",
     location: {
         lat: 44.399431,
         lng: 26.133342
     }
 }, {
     title: "Plumbuita Park",
+    sector: "Sector 2",
     location: {
         lat: 44.469806,
         lng: 26.135359
     }
 }, {
     title: "Bordei Park",
+    sector: "Sector 2",
     location: {
         lat: 44.471531,
         lng: 26.093903
     }
 }, {
     title: "Herastrau Park",
+    sector: "Sector 1",
     location: {
         lat: 44.479186,
         lng: 26.080599
     }
 }, {
     title: "Bazilescu Park",
+    sector: "Sector 1",
     location: {
         lat: 44.488668,
         lng: 26.034508
     }
 }, {
     title: "Kiseleff Park",
+    sector: "Sector 1",
     location: {
         lat: 44.457677,
         lng: 26.083603
     }
 }, {
     title: "Cismigiu Park",
+    sector: "Sector 1",
     location: {
         lat: 44.436660,
         lng: 26.091070
     }
 }, {
     title: "Botanic Garden",
+    sector: "Sector 6",
     location: {
         lat: 44.437273,
         lng: 26.062660
     }
 }, {
     title: "The Youth Park",
+    sector: "Sector 4",
     location: {
         lat: 44.406878,
         lng: 26.104460
     }
 }, {
     title: "Carol Park",
+    sector: "Sector 5",
     location: {
         lat: 44.413183,
         lng: 26.095533
     }
 }, {
     title: "Drumul Taberei Park",
+    sector: "Sector 6",
     location: {
         lat: 44.420024,
         lng: 26.032190
     }
 }, {
     title: "Giulesti Park",
+    sector: "Sector 6",
     location: {
         lat: 44.460531,
         lng: 26.043005
     }
 }, {
     title: "Crangasi Park",
+    sector: "Sector 6",
     location: {
         lat: 44.451735,
         lng: 26.043692
     }
 }, {
     title: "Pantelimon Park",
+    sector: "Sector 3",
     location: {
         lat: 44.436880,
         lng: 26.203208
     }
 }, {
     title: "Constantin Brancusi Park",
+    sector: "Sector 3",
     location: {
         lat: 44.429306,
         lng: 26.164799
     }
 }, {
     title: "Gheorghe Patrascu Park",
+    sector: "Sector 2",
     location: {
         lat: 44.431298,
         lng: 26.149778
     }
 }, {
     title: "Opera Park",
+    sector: "Sector 6",
     location: {
         lat: 44.436064,
         lng: 26.077273
     }
 }, {
     title: "Izvor Park",
+    sector: "Sector 5",
     location: {
         lat: 44.431637,
         lng: 26.087916
     }
 }, {
     title: "Grozavesti Park",
+    sector: "Sector 6",
     location: {
         lat: 44.436203,
         lng: 26.056416
     }
 }, {
     title: "Queen Marie Park",
+    sector: "Sector 1",
     location: {
         lat: 44.457815,
         lng: 26.066008
