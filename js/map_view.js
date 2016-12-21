5037ce65975b1fc7484126ce4a8bf2473db70dd7
Daniel Nedelcu <nede.dan@gmail.com>
2016-12-21 18:46:55 +0200

    first attempt at wikipedia api

--- js/map_view.js
+++ js/map_view.js
@@ -1,4 +1,5 @@
 var map;
+  var wikiURL;
 
 //This is the function that initializez the map
 var initializeMap = function() {
@@ -17,6 +18,8 @@ var Park = function (data) {
     "use strict";
     this.title = ko.observable(data.title);
     this.position = ko.observable(data.position);
+    this.lat = ko.observable(data.position.lat);
+    this.lng = ko.observable(data.position.lng);
     this.count = ko.observable(data.count);
     this.marker = ko.observable(data.marker);
     this.sector = ko.observable(data.sector);
@@ -31,7 +34,7 @@ var ViewModel = function() {
   self.markerList = ko.observableArray([]);
 
   //create info window with InfoWindow() method
-  var infoWindow = new google.maps.InfoWindow();
+  var infowindow = new google.maps.InfoWindow();
 
   //iterates through the initial park array (from parksDB.js file) and pushes each array item
   //into the markerList observable array
@@ -42,6 +45,8 @@ var ViewModel = function() {
 
   var marker;
   var position;
+  var infoContent;
+
 
   //Clears all markers from map
   this.clearMap = function() {
@@ -53,7 +58,7 @@ var ViewModel = function() {
     });
   };
 
-  this.parkSectors = ko.observableArray([1, 2, 3, 4, 5, 6]);
+  this.parkSectors = ko.observableArray(["Sector 1", "Sector 2", "Sector 3", "Sector 4", "Sector 5", "Sector 6"]);
 
   //TODO: it is suposed to hold the value from the selected sector
   this.selectedSector = ko.observableArray([]);
@@ -73,30 +78,63 @@ var ViewModel = function() {
          }
      });
 
-  //creates google maps marker for each new park
-  this.parksBySector().forEach(function (parkItem) {
-    var position = parkItem.position();
-    var title = parkItem.title();
-      marker = new google.maps.Marker({
-          position: position,
-          title: title,
-          map: map,
-          icon: new google.maps.MarkerImage('img/park.png'),
-          animation: google.maps.Animation.DROP,
-      });
-      parkItem.marker = marker;
+     //creates google maps marker for each new park
+     this.parksBySector().forEach(function (parkItem) {
+       var position = parkItem.position();
+       var title = parkItem.title();
+       var lat = parkItem.position.lat;
+       var lng = parkItem.position.lng;
+       wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + title + "&format=json&callback=wikiCallback";
+       //infoContent = infos;
+         marker = new google.maps.Marker({
+             position: position,
+             title: title,
+             lat: lat,
+             lng: lng,
+             infoContent: infoContent,
+             map: map,
+             icon: new google.maps.MarkerImage('img/park.png'),
+             animation: google.maps.Animation.DROP,
+         });
+         parkItem.marker = marker;
 
-      self.clicker = function() {
-        displayInfoWindow(this.marker, infoWindow);
-      };
+         self.clicker = function() {
+           displayInfoWindow(this.marker, infowindow);
+         };
 
-      marker.addListener('click', function() {
-        displayInfoWindow(this, infoWindow);
-      });
-  });
+         marker.addListener('click', function() {
+           displayInfoWindow(this, infowindow);
+         });
+
+     });
+
+
+
+  /*client id and client secret for foursquare api*/
+  var CLIENT_ID_Foursquare = 'NWYE5SXY1XOXPBCEMAJJKJRDMQFLEVGEN1YQPU3EUIR02RFV';
+  var CLIENT_SECRET_Foursquare = 'NMEUQSYSLXU4UUDO1YHKBTMIXH3GXT1GHRFNJ13HVQATCCUN';
+  /**creating all the markers on the map**/
 
 } //ViewModel() end
 
+function getWiki() {
+  $.ajax({
+    url: wikiURL,
+    dataType: "jsonp",
+    success: function (response) {
+      var articleList = response[1];
+      for (var i = 0; i < articleList.lenght; i++) {
+        articleStr = articleList[i];
+        var url = "http://en.wikipedia.org/wiki/" + articleStr;
+
+        infoContent ='<li><a href=' + '"' + url + '">' + articleStr + '</a></li>';
+      };
+
+    }
+  });
+  return false;
+}
+getWiki();
 
 //animates marker and loads information in an InfoWindow();
 var displayInfoWindow = function(marker, infowin) {
@@ -107,10 +145,13 @@ var displayInfoWindow = function(marker, infowin) {
     }, 1450);
     map.panTo(marker.position);
     infowin.marker = marker;
-    infowin.setContent('<div>' + marker.title + '</div>');
+    infowin.setContent('<div>' + marker.infoContent + '</div>');
     infowin.open(map, marker);
     infowin.addListener('closeclick', function(){
       infowin.setMap(null);
     });
   };
 };
+
+var wikiElem;
+//var wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + marker.title + "&format=json&callback=wikiCallback";
